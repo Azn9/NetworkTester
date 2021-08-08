f2613650845ab464a741ab55f6ea5bb11c55b85a
Axel Joly <azn9.pro@gmail.com>
2021-08-08 15:49:35 +0200

    Move to InfluxDb

--- src/main/java/dev/azn9/nt/Main.java
+++ src/main/java/dev/azn9/nt/Main.java
@@ -1,25 +1,40 @@
 package dev.azn9.nt;
 
+import com.influxdb.client.InfluxDBClient;
+import com.influxdb.client.InfluxDBClientFactory;
+import com.influxdb.client.WriteApi;
+import com.influxdb.client.domain.WritePrecision;
+import com.influxdb.client.write.Point;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.InetAddress;
-import java.net.MalformedURLException;
-import java.net.SocketTimeoutException;
 import java.net.URL;
 import java.net.UnknownHostException;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
+import java.util.UUID;
 
 public class Main {
 
-    private static boolean does8ping = true;
-    private static boolean does1ping = true;
+    private static final String token = System.getenv("INFLUX_TOKEN");
+    private static final String bucket = "data";
+    private static final String org = "azn9";
+    private static final Map<UUID, Boolean> threadStatus = new HashMap<>();
+    private static final Map<UUID, Thread> threads = new HashMap<>();
+
+    private static WriteApi writeApi;
+    private static Logger logger;
 
     public static void main(String[] args) {
-        Logger logger = LogManager.getLogger(Main.class);
+        InfluxDBClient client = InfluxDBClientFactory.create("http://localhost:8086", token.toCharArray());
+        Main.writeApi = client.getWriteApi();
+        Main.logger = LogManager.getLogger(Main.class);
 
         logger.info("");
         logger.info("=================");
@@ -29,22 +44,10 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        Thread testThread = new Thread(() -> {
-            while (true) {
-                try {
-                    Main.loop(logger);
-                } catch (Exception exception) {
-                    logger.error("Error", exception);
-                }
-
-                try {
-                    Thread.sleep(2000);
-                } catch (InterruptedException ignored) {
-                }
-            }
-        });
-        testThread.setDaemon(true);
-        testThread.start();
+        Main.createTest(false, "8.8.8.8", 200);
+        Main.createTest(false, "1.1.1.1", 200);
+        Main.createTest(true, "https://example.com", 200);
+        Main.createTest(true, "https://api.twitch.tv/helix/streams/", 401);
 
         while (true) {
             String input;
@@ -66,88 +69,80 @@ public class Main {
             }
         }
 
-        testThread.interrupt();
+        new HashMap<>(Main.threadStatus).forEach((uuid, aBoolean) -> {
+            Main.threadStatus.put(uuid, false);
+            try {
+                Main.threads.remove(uuid).join();
+            } catch (NullPointerException ignored) {
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
 
         logger.traceExit();
+        client.close();
 
         System.exit(0);
     }
 
-    private static void loop(Logger logger) throws MalformedURLException {
-        logger.trace("Pinging 8.8.8.8...");
-        long currentMilis = System.currentTimeMillis();
-        InetAddress inet = null;
-        try {
-            inet = InetAddress.getByName("8.8.8.8");
-        } catch (UnknownHostException exception) {
-            logger.fatal("Unknown host 8.8.8.8", exception);
-        }
+    private static void createTest(boolean isUrl, String host, int code) {
+        UUID threadUuid = UUID.randomUUID();
 
-        try {
-            if (inet != null && !inet.isReachable(1000)) {
-                if (Main.does8ping) {
-                    logger.fatal("Lost connection to 8.8.8.8 !");
-                    Main.does8ping = false;
+        Thread thread = new Thread(() -> {
+            while (Main.threadStatus.getOrDefault(threadUuid, true)) {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException ignored) {
+                }
+
+                long currentMilis = System.currentTimeMillis();
+
+                if (isUrl) {
+                    try {
+                        URL url = new URL(host);
+                        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                        connection.setRequestMethod("GET");
+                        connection.setConnectTimeout(5000);
+                        connection.setReadTimeout(5000);
+
+                        if (connection.getResponseCode() != code) {
+                            throw new IOException("Invalid response code");
+                        }
+                    } catch (Exception e) {
+                        logger.fatal("Error " + host, e);
+                        continue;
+                    }
                 } else {
-                    logger.info("8.8.8.8 is alive !");
-                    Main.does8ping = true;
+                    InetAddress inet;
+                    try {
+                        inet = InetAddress.getByName("8.8.8.8");
+                    } catch (UnknownHostException exception) {
+                        logger.fatal("Unknown host " + host, exception);
+                        continue;
+                    }
+
+                    try {
+                        inet.isReachable(5000);
+                    } catch (IOException exception) {
+                        logger.fatal("IOE " + host, exception);
+                        continue;
+                    }
                 }
+
+                Point point = Point
+                        .measurement("ping")
+                        .addTag("host", host)
+                        .addField("rtt", System.currentTimeMillis() - currentMilis)
+                        .time(Instant.now(), WritePrecision.MS);
+
+                Main.writeApi.writePoint(bucket, org, point);
             }
-        } catch (IOException exception) {
-            logger.fatal("IOE 8.8.8.8", exception);
-        }
-        logger.trace("8.8.8.8 - RTT : " + (System.currentTimeMillis() - currentMilis) + "ms");
+        });
+        thread.setDaemon(true);
+        thread.start();
 
-        logger.trace("Pinging 1.1.1.1...");
-        currentMilis = System.currentTimeMillis();
-        inet = null;
-        try {
-            inet = InetAddress.getByName("1.1.1.1");
-        } catch (UnknownHostException exception) {
-            logger.fatal("Unknown host 1.1.1.1", exception);
-        }
-
-        try {
-            if (inet != null && !inet.isReachable(1000)) {
-                if (Main.does1ping) {
-                    logger.fatal("Lost connection to 1.1.1.1 !");
-                    Main.does1ping = false;
-                } else {
-                    logger.info("1.1.1.1 is alive !");
-                    Main.does1ping = true;
-                }
-            }
-        } catch (IOException exception) {
-            logger.fatal("IOE 1.1.1.1", exception);
-        }
-        logger.trace("1.1.1.1 - RTT : " + (System.currentTimeMillis() - currentMilis) + "ms");
-
-        logger.trace("Trying to get https://example.com/");
-        currentMilis = System.currentTimeMillis();
-        URL url = new URL("https://example.com/");
-        HttpURLConnection connection = null;
-        try {
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.setConnectTimeout(2000);
-            connection.setReadTimeout(2000);
-        } catch (SocketTimeoutException timeoutException) {
-            logger.fatal("Connection timed out !", timeoutException);
-        } catch (IOException exception) {
-            logger.fatal("IOE", exception);
-        }
-
-        if (connection != null) {
-            try {
-                int code;
-                if ((code = connection.getResponseCode()) != 200) {
-                    throw new IOException("Response code: " + code);
-                }
-            } catch (IOException exception) {
-                logger.fatal("IOE2", exception);
-            }
-        }
-        logger.trace("WEBSITE - RTT : " + (System.currentTimeMillis() - currentMilis) + "ms");
+        Main.threadStatus.put(threadUuid, true);
+        Main.threads.put(threadUuid, thread);
     }
 
 }
